b8b64bb6a67fd0c629696596822bdd1376b56016
ellevalentine <elle.valentine@hotmail.com>
2019-04-29 14:43:27 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,46 @@
 Rails.application.routes.draw do
+
+  resources :songs
+  resources :genres
+  resources :artists
+
+
+
+  # get 'songs/index'
+  #
+  # get 'songs/show'
+  #
+  # get 'songs/new'
+  #
+  # get 'songs/create'
+  #
+  # get 'songs/edit'
+  #
+  # get 'songs/update'
+  #
+  # get 'genres/index'
+  #
+  # get 'genres/show'
+  #
+  # get 'genres/new'
+  #
+  # get 'genres/create'
+  #
+  # get 'genres/edit'
+  #
+  # get 'genres/update'
+  #
+  # get 'artists/index'
+  #
+  # get 'artists/show'
+  #
+  # get 'artists/new'
+  #
+  # get 'artists/create'
+  #
+  # get 'artists/edit'
+  #
+  # get 'artists/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
